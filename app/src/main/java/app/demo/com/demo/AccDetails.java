530048f00f0a18ac56fbc90835df2cc6721c8122
Jatin Bhardwaj <31664635+jatinbharadwaj@users.noreply.github.com>
2018-10-16 04:25:36 +0530

    Java Files

---
+++ app/src/main/java/app/demo/com/demo/AccDetails.java
@@ -0,0 +1,21 @@
+package app.demo.com.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class AccDetails extends AppCompatActivity {
+    TextView txtName,txtEmailId,txtPhoneNo,txtLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acc_details);
+
+        txtName=(TextView) findViewById(R.id.txtName);
+        txtEmailId=(TextView) findViewById(R.id.txtEmailId);
+        txtLocation=(TextView) findViewById(R.id.txtLocation);
+        txtPhoneNo=(TextView) findViewById(R.id.txtPhoneNo);
+
+    }
+}
---
+++ app/src/main/java/app/demo/com/demo/SignUp.java
@@ -0,0 +1,27 @@
+package app.demo.com.demo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+//asks details and send to firebase at idChild
+//NAME for name
+//E-MAIL for email
+//PASSWORD for password
+//PHONE for phone
+//after sign up move to LocationDetection(in actual interface)
+
+
+public class SignUp extends AppCompatActivity {
+    private EditText edtName,edtEmail,edtPhNo,edtPwd,edtRePwd;
+    private Button btnsignup2;
+
+}
+
+   
+
+
