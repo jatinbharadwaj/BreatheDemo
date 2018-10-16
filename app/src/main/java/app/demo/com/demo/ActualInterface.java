b173881586054e3ed61ab6a0cad876ccd2c65147
Jatin Bhardwaj <31664635+jatinbharadwaj@users.noreply.github.com>
2018-10-17 00:36:11 +0530

    R Error resolved

--- app/src/main/java/app/demo/com/demo/ActualInterface.java
+++ app/src/main/java/app/demo/com/demo/ActualInterface.java
@@ -1,19 +1,21 @@
 package app.demo.com.demo;
 
+
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class Actual_Interface extends AppCompatActivity {
+public class ActualInterface extends AppCompatActivity {
 
     private Button btnForecast,btnSearchLoc,btnShopNow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_actual__interface);
+        setContentView(R.layout.activity_actual_interface);
 
         btnSearchLoc=(Button) findViewById(R.id.btnSearchLoc);
         btnForecast=(Button) findViewById(R.id.btnForecast);
@@ -23,7 +25,7 @@ public class Actual_Interface extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //go to forecast activity
-                Intent intentToForecast=new Intent(Actual_Interface.this,forecast.class);
+                Intent intentToForecast=new Intent(ActualInterface.this,forecast.class);
                 startActivity(intentToForecast);
             }
         });
--- app/src/main/java/app/demo/com/demo/MainActivity.java
+++ app/src/main/java/app/demo/com/demo/MainActivity.java
@@ -1,4 +1,19 @@
 package app.demo.com.demo;
 
-public class MainActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
 }
