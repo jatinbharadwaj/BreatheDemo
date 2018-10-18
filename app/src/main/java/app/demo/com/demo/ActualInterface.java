ff13486dece9d3370e33cda81cd1898fb77f95b1
Jatin Bhardwaj <31664635+jatinbharadwaj@users.noreply.github.com>
2018-10-19 03:22:21 +0530

    Added profile icon in Action Bar

--- app/src/main/java/app/demo/com/demo/ActualInterface.java
+++ app/src/main/java/app/demo/com/demo/ActualInterface.java
@@ -5,6 +5,9 @@ package app.demo.com.demo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -36,5 +39,38 @@ public class ActualInterface extends AppCompatActivity {
                 //go to ShopNow activity
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater =getMenuInflater();
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        // if (id == R.id.profile_settings) {
+        //   return true;
+        //}
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+
+            case R.id.profile_settings:
+                Intent intent = new Intent(ActualInterface.this, app.demo.com.demo.AccDetails.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return false;
+        }
     }
 }
